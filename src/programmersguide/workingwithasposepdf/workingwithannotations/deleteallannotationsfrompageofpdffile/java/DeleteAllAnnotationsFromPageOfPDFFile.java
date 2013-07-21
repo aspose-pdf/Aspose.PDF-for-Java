c3612a2caf3e532249cea272b6a34985b8ac49bb
Fatima Ahmad <fatima.rabbiya@aspose.com>
2013-07-21 23:04:28 +0500

    Added New Examples

---
+++ src/programmersguide/workingwithasposepdf/workingwithannotations/deleteallannotationsfrompageofpdffile/java/DeleteAllAnnotationsFromPageOfPDFFile.java
@@ -0,0 +1,36 @@
+/* 
+ * Copyright 2001-2013 Aspose Pty Ltd. All Rights Reserved.
+ *
+ * This file is part of Aspose.Pdf. The source code in this file
+ * is only intended as a supplement to the documentation, and is provided
+ * "as is", without warranty of any kind, either expressed or implied.
+ */
+ 
+package programmersguide.workingwithasposepdf.workingwithannotations.deleteallannotationsfrompageofpdffile.java;
+
+import com.aspose.pdf.*;
+
+public class DeleteAllAnnotationsFromPageOfPDFFile
+{
+    public static void main(String[] args) throws Exception
+    {
+        // The path to the documents directory.
+        String dataDir = "src/programmersguide/workingwithasposepdf/workingwithannotations/deleteallannotationsfrompageofpdffile/data/";
+
+        //open source PDF document
+        com.aspose.pdf.Document pdfDocument = new com.aspose.pdf.Document(dataDir + "input.pdf");
+
+        //delete particular annotation
+        pdfDocument.getPages().get_Item(1).getAnnotations().delete();
+
+        // save the update document
+        pdfDocument.save(dataDir + "output.pdf");
+
+        // Display result.
+        System.out.println("Process Completed Successfully!");
+    }
+}
+
+
+
+
