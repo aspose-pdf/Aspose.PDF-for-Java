118f487cc0ac0cfb1941db69f4bd9af6d10e7793
Fatima Ahmad <fatima.rabbiya@aspose.com>
2013-08-20 11:30:04 +0500

    Added New Examples

---
+++ src/programmersguide/workingwithasposepdf/workingwithannotations/setformattingoffreetextannotation/java/SetFormattingOfFreeTextAnnotation.java
@@ -0,0 +1,41 @@
+/* 
+ * Copyright 2001-2013 Aspose Pty Ltd. All Rights Reserved.
+ *
+ * This file is part of Aspose.Pdf. The source code in this file
+ * is only intended as a supplement to the documentation, and is provided
+ * "as is", without warranty of any kind, either expressed or implied.
+ */
+ 
+package programmersguide.workingwithasposepdf.workingwithannotations.setformattingoffreetextannotation.java;
+
+import com.aspose.pdf.*;
+
+public class SetFormattingOfFreeTextAnnotation
+{
+    public static void main(String[] args) throws Exception
+    {
+        // The path to the documents directory.
+        String dataDir = "src/programmersguide/workingwithasposepdf/workingwithannotations/setformattingoffreetextannotation/data/";
+
+        //open document
+        Document pdfDocument = new Document(dataDir + "input.pdf");
+
+        //create annotation
+        FreeTextAnnotation freetext = new FreeTextAnnotation(pdfDocument.getPages().get_Item(1), new com.aspose.pdf.Rectangle(200, 300, 400, 500),"/Arial 48 Tf 1 0 0 rg");
+
+        freetext.setContents("Free Text");
+
+        // add anootation to annotations collection of page
+        pdfDocument.getPages().get_Item(1).getAnnotations().add(freetext);
+
+        // save the updated document
+        pdfDocument.save(dataDir + "output.pdf");
+
+        // Display result.
+        System.out.println("Process completed Successfully!");
+    }
+}
+
+
+
+
