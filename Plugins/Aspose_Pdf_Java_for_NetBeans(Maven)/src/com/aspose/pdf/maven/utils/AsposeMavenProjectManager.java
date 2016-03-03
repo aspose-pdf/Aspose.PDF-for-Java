c672a3b0dc3d19a9d9362e29c1e9dbe1270d7ac4
ali_salman <ali_salman_baloch@hotmail.com>
2016-03-03 16:39:27 +0500

    Updated copyright year

--- Plugins/Aspose_Pdf_Java_for_NetBeans(Maven)/src/com/aspose/pdf/maven/utils/AsposeMavenProjectManager.java
+++ Plugins/Aspose_Pdf_Java_for_NetBeans(Maven)/src/com/aspose/pdf/maven/utils/AsposeMavenProjectManager.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 1998-2015 Aspose Pty Ltd.
+ * Copyright (c) 1998-2016 Aspose Pty Ltd.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -44,12 +44,6 @@ import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 import org.openide.xml.XMLUtil;
 import org.w3c.dom.Node;
-
-/*
-* @author Adeel Ilyas <adeel.ilyas@aspose.com>
-* Date: 12/21/2015
-*
- */
 
 /**
  *
