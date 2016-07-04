9082a198d69eb8025d4985e255dcc93f27440df9
Aspose.Pdf Product Family <pdf@aspose.com>
2016-07-04 23:17:48 +0500

    Removed Copyright Message.

--- Examples/src/main/java/com/aspose/pdf/examples/AsposePdf/Forms/MoveFormField.java
+++ Examples/src/main/java/com/aspose/pdf/examples/AsposePdf/Forms/MoveFormField.java
@@ -1,11 +1,3 @@
-/* 
- * Copyright 2001-2015 Aspose Pty Ltd. All Rights Reserved.
- *
- * This file is part of Aspose.Pdf. The source code in this file
- * is only intended as a supplement to the documentation, and is provided
- * "as is", without warranty of any kind, either expressed or implied.
- */
- 
 package com.aspose.pdf.examples.AsposePdf.Forms;
 
 import com.aspose.pdf.examples.Utils;
